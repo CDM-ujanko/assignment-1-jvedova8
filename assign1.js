406769699a9ca4bcce2d1cc3d610f0f034c5bf45
jvedova8 <jvedova.dp@gmail.com>
2019-04-15 17:38:53 -0500

    second commit

---
+++ assign1.js
@@ -0,0 +1,9 @@
+console.log('Assignment One New File!');
+
+$(document).ready;
+console.log(document);
+console.log($);
+
+$( document ).ready(function() {
+    console.log( 'JQuery is ready!' );
+    });
